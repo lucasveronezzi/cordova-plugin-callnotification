68d6049c2bc9dccf1668c30451da2ab04816a13a
Lucas Veronezzi <lucas.veronezzi@buenonetworks.com.br>
2020-07-08 21:25:16 -0300

    Add icon to notification call full screen

--- src/android/ReceveingCallActivity.java
+++ src/android/ReceveingCallActivity.java
@@ -4,11 +4,25 @@ import android.app.Activity;
 import android.app.KeyguardManager;
 import android.app.NotificationManager;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.os.Bundle;
 import android.os.Build;
 import android.view.View;
 import android.view.WindowManager;
 import android.content.Context;
+import android.widget.ImageView;
+
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 
 public class ReceveingCallActivity extends Activity  {
 
@@ -52,7 +66,23 @@ public class ReceveingCallActivity extends Activity  {
 
         setContentView(res.getIdentifier("activity_receveing_call", "layout", package_name));
 
+        ImageView userIcon = (ImageView) findViewById(res.getIdentifier("userIcon", "id", package_name));
 
+        Glide.with(userIcon.getContext())
+          .asBitmap()
+          .load("https://homolog.consaudeonline.com.br/img/user_default.png")
+          .apply(RequestOptions
+            .circleCropTransform()
+            .placeholder(res.getIdentifier("icon_user_round", "drawable", package_name))
+          )
+          .into(new BitmapImageViewTarget(userIcon) {
+            @Override
+            protected void setResource(Bitmap resource) {
+              RoundedBitmapDrawable circularBitmapDrawable = RoundedBitmapDrawableFactory.create(userIcon.getContext().getResources(), addBorder(resource, userIcon.getContext()));
+              circularBitmapDrawable.setCircular(true);
+              userIcon.setImageDrawable(circularBitmapDrawable);
+            }
+          });
 
         CallNotification.startVibration(this);
     }
@@ -81,5 +111,28 @@ public class ReceveingCallActivity extends Activity  {
         CallNotification.createMainActivy(this, true);
 
         finish();
+    }
+
+    private static Bitmap addBorder(Bitmap resource, Context context) {
+      if(resource == null) return resource;
+
+      int w = resource.getWidth();
+      int h = resource.getHeight();
+      int radius = Math.min(h / 2, w / 2);
+      Bitmap output = Bitmap.createBitmap(w + 8, h + 8, Bitmap.Config.ARGB_8888);
+      Paint p = new Paint();
+      p.setAntiAlias(true);
+      Canvas c = new Canvas(output);
+      c.drawARGB(0, 0, 0, 0);
+      p.setStyle(Paint.Style.FILL);
+      c.drawCircle((w / 2) + 4, (h / 2) + 4, radius, p);
+      p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+      c.drawBitmap(resource, 4, 4, p);
+      p.setXfermode(null);
+      p.setStyle(Paint.Style.STROKE);
+      p.setColor(ContextCompat.getColor(context, android.R.color.white));
+      p.setStrokeWidth(5);
+      c.drawCircle((w / 2) + 4, (h / 2) + 4, radius, p);
+      return output;
     }
 }
